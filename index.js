265555f38066281be56c77f5af7f9f0f98f6de0c
kevin murphy <kevinnealmurphy@gmail.com>
2020-12-11 04:13:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries  = batteryBatches.reduce(batch => {
+  let result += batch
+}, 0)
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
